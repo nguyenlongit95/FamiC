bd8b4572c3fc1e9ff5221ec4058e06e8c1afd3fe
Nguyen Cong Thai Long <nguyenlongit95@Nguyens-MacBook-Air.local>
2018-09-19 21:12:55 +0700

    Create Project FamiC

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+ * Taị đây chứa các Route cơ bản
+ * Route cho phía admin
+ * CURD cho các thành phần của hệ thống
+ * Create
+ * Update
+ * Delete
+ * show
+ * index
+ * */
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('DashBoard','adminController@DashBoard');
+    Route::group(['prefix'=>'Category'],function(){
+        Route::get('Categories','CategoryController@index');
+    });
+});
